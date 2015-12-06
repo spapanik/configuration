6ddd93124c42203318960ab0ce35c47fa3520909
stephanos papanik <spapanik21@gmail.com>
2015-12-06 20:29:58 +0000

    sane history

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -2,8 +2,6 @@ HISTSIZE=100000
 SAVEHIST=100000
 HISTFILE=~/.zsh_history
 
-setopt hist_expire_dups_first
 setopt hist_ignore_all_dups
-setopt hist_ignore_space
-setopt inc_append_history
-setopt share_history
+setopt hist_reduce_blanks
+setopt append_history
