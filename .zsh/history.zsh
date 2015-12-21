b9404c1382e387e069c5d65d4b1af06adc9af46c
steph papanik <stephanos.papanikolopoulos@smarkets.com>
2015-12-21 12:56:13 +0000

    better history for sharing

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -2,6 +2,6 @@ HISTSIZE=100000
 SAVEHIST=100000
 HISTFILE=~/.zsh_history
 
-setopt hist_ignore_all_dups
+setopt hist_save_no_dups
 setopt hist_reduce_blanks
-setopt append_history
+setopt inc_append_history
