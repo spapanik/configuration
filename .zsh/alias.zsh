9e7d6489a7cf6c0050a153700ba73d37c25a9fbc
steph papanik <stephanos.papanikolopoulos@smarkets.com>
2015-12-22 13:37:54 +0000

    alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,5 +1,6 @@
 alias ls="ls --color"
 alias ll="ls --color -l"
 alias manage="python manage.py"
-alias grepc="grep --color=auto"
+alias grep="grep --color=auto"
+alias tmux="tmux -2"
 pb () { curl -F "c=@${1:--}" https://ptpb.pw/ }
