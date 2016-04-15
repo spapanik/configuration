b243c155633fe635f2043fb0cd0d054d2885c8a8
steph papanik <spapanik21@gmail.com>
2016-04-15 07:33:38 +0100

    remove unneeded if

--- .zsh/keybinding.zsh
+++ .zsh/keybinding.zsh
@@ -1,13 +1,14 @@
-if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
-    function zle-line-init () {
-        printf '%s' "${terminfo[smkx]}"
-    }
-    function zle-line-finish () {
-        printf '%s' "${terminfo[rmkx]}"
-    }
-    zle -N zle-line-init
-    zle -N zle-line-finish
-fi
+function zle-line-init () {
+    printf '%s' "${terminfo[smkx]}"
+}
+
+function zle-line-finish () {
+    printf '%s' "${terminfo[rmkx]}"
+}
+
+zle -N zle-line-init
+
+zle -N zle-line-finish
 
 stty stop '' -ixoff
 stty stop '' -ixon
