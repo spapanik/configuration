0045549ff6b62beb0b76972758195b332dc76ad8
steph papanik <spapanik21@gmail.com>
2016-10-19 11:35:24 +0100

    add flags to less

--- .zsh/constants.zsh
+++ .zsh/constants.zsh
@@ -1,5 +1,6 @@
-export LSCOLORS="Gxfxcxdxbxegedabagacad"
-export PYTHONSTARTUP="$HOME/.pyrc"
-export EDITOR="vim"
-export VISUAL="vim"
+export LSCOLORS='Gxfxcxdxbxegedabagacad'
+export PYTHONSTARTUP='$HOME/.pyrc'
+export EDITOR='vim'
+export VISUAL='vim'
 export TERM='xterm-256color'
+export LESS='-FRXK'
