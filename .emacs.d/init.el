ec413b40a542725c2d156184dda3142a035623a1
Steve Papanik <spapanik21@gmail.com>
2017-03-01 22:42:39 +0000

    Add directory tree to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,7 @@
 (setq inhibit-startup-message t)
 (setq-default column-number-mode t)
 (setq display-time-24hr-format t)
-(display-time)
+(display-time-mode 1)
 (setq require-final-newline t)
 (unless window-system
   (menu-bar-mode -1)
@@ -39,9 +39,13 @@
   :ensure t)
 (use-package tramp
   :ensure t)
+(use-package direx
+  :ensure t)
 
 (global-set-key (kbd "M-n") 'mc/mark-next-like-this)
 (global-set-key (kbd "M-p") 'mc/mark-previous-like-this)
+
+(global-set-key (kbd "C-x C-j") 'direx:jump-to-directory)
 
 ;; move lines
 (defun move-line-up ()
@@ -65,3 +69,17 @@
 (global-set-key (kbd "M-<right>") 'windmove-right)
 (global-set-key (kbd "M-<up>") 'windmove-up)
 (global-set-key (kbd "M-<down>") 'windmove-down)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (direx yasnippet use-package smart-tab multiple-cursors darcula-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
