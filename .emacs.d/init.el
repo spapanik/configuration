901a3f1854ff3782de5b5b6d2f7db8ebfbec579e
Steve Papanik <spapanik21@gmail.com>
2017-03-12 10:31:26 +0000

    Remove custom commands

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -69,17 +69,3 @@
 (global-set-key (kbd "M-<right>") 'windmove-right)
 (global-set-key (kbd "M-<up>") 'windmove-up)
 (global-set-key (kbd "M-<down>") 'windmove-down)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (direx yasnippet use-package smart-tab multiple-cursors darcula-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
