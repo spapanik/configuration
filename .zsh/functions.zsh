31dca5f532720db1e49a273e97b73c826766cb4b
Steph Papanik <spapanik21@gmail.com>
2017-03-30 16:04:58 +0100

    Regex checkout in git

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -4,3 +4,11 @@ pb () { curl -F "c=@${1:--}" https://ptpb.pw/ }
 glb () {
     git branch -vv --no-color | grep --color=never ] | awk '{print substr($0,3);}' | awk '{$2=""; print $0}'| awk -F']' '{print $1}'| awk -F':|\\[' '{$2=""; if ($3=="") print $0; else print $1,"\033[1;31m"$3"\033[0m ";}' && git branch -vv --no-color | grep --color=never -v ] | awk '{print substr($0,3);}' | awk '{print $1,"\033[1;31mlocal only\033[0m "}'
 }
+gcg () {
+    MATCHES=( $(git branch --no-color | cut -c 3- | grep $@) )
+    case ${#MATCHES[@]} in
+        0) echo "No branches matched '$@'";;
+        1) git checkout ${MATCHES[1]};;
+        *) echo "More than one branches matched '$@'";;
+    esac
+}
