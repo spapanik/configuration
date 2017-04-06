4c16dc736966f7c0c0c4830ad8a85fb0419066b7
Steph Papanik <spapanik21@gmail.com>
2017-04-06 10:56:14 +0100

    Move lb to git to git aliases

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,9 +1,6 @@
-manage_local() {python manage.py ${@} --settings=settings.local} 
-manage_test() {python manage.py ${@} --settings=settings.test} 
+manage_local() {python manage.py ${@} --settings=settings.local}
+manage_test() {python manage.py ${@} --settings=settings.test}
 pb () { curl -F "c=@${1:--}" https://ptpb.pw/ }
-glb () {
-    git branch -vv --no-color | grep --color=never ] | awk '{print substr($0,3);}' | awk '{$2=""; print $0}'| awk -F']' '{print $1}'| awk -F':|\\[' '{$2=""; if ($3=="") print $0; else print $1,"\033[1;31m"$3"\033[0m ";}' && git branch -vv --no-color | grep --color=never -v ] | awk '{print substr($0,3);}' | awk '{print $1,"\033[1;31mlocal only\033[0m "}'
-}
 gcg () {
     MATCHES=( $(git branch --no-color | cut -c 3- | grep $@) )
     case ${#MATCHES[@]} in
