ad256c05d1deec22f372318f5371a39e06557bd4
Steph Papanik <spapanik21@gmail.com>
2017-04-06 11:08:41 +0100

    Move cg to gitconfig

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,11 +1,3 @@
 manage_local() {python manage.py ${@} --settings=settings.local}
 manage_test() {python manage.py ${@} --settings=settings.test}
 pb () { curl -F "c=@${1:--}" https://ptpb.pw/ }
-gcg () {
-    MATCHES=( $(git branch --no-color | cut -c 3- | grep $@) )
-    case ${#MATCHES[@]} in
-        0) echo "No branches matched '$@'";;
-        1) git checkout ${MATCHES[1]};;
-        *) echo "More than one branches matched '$@'";;
-    esac
-}
