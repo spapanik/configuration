49cdf0066fd69bbdc2dcbbf9924fa204bae77e74
Steph Papanik <spapanik21@gmail.com>
2017-04-20 22:59:03 +0100

    Better colors

--- .zsh/theme.zsh
+++ .zsh/theme.zsh
@@ -1,25 +1,24 @@
-export LSCOLORS='Gxfxcxdxbxegedabagacad'
 local reset=${reset_color}
-local failure=${fg_bold[red]}
-local warning=${fg_bold[yellow]}
-local success=${fg_bold[green]}
-local branch=${fg[red]}
-local git=${fg_bold[blue]}
-local directory=${fg[cyan]}
+local failure={160}
+local warning={136}
+local success={64}
+local branch={125}
+local git={33}
+local directory={37}
 
 if [[ -a $HOME/.light ]]; then
-    local user_info=${fg_no_bold[black]}
+    local user_info={240}
 else
-    local user_info=${fg_no_bold[white]}
+    local user_info={245}
 fi
 
-local ret_status="%(?:%{${success}%}:%{${failure}%}%s)"
-local first_line="%{${directory}%}%~ "
-local second_line="%{${user_info}%}%n@%M${ret_status}$%{${reset}%} "
+local ret_status="%(?:%{%F${success}%}:%{%F${failure}%}%s)"
+local first_line="%{%F${directory}%}%~ "
+local second_line="%{%F${user_info}%}%n@%M${ret_status}$%{${reset}%} "
 PROMPT='${first_line}$(git_prompt_info)
 ${second_line}'
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{${git}%}git:(%{${branch}%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{${git}%})"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{${git}%}) %{${warning}%}✗"
+ZSH_THEME_GIT_PROMPT_PREFIX="%{%F${git}%}git:(%{%F${branch}%}"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{%F${git}%})"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{%F${git}%}) %{%F${warning}%}✗"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{${reset}%}"
