5a942f5502b7c6a0a6be204faf72099b31dce4e0
Steve Papanik <spapanik21@gmail.com>
2017-04-22 08:26:12 +0100

    custom python venv

--- .zsh/theme.zsh
+++ .zsh/theme.zsh
@@ -1,25 +1,66 @@
-autoload colors; colors;
-local reset=${reset_color}
-local failure={160}
-local warning={136}
-local success={64}
-local branch={125}
-local git={33}
-local directory={37}
+# ANSI colors
+local black="%{${fg[black]}%}"
+local red="%{${fg[red]}%}"
+local green="%{${fg[green]}%}"
+local yellow="%{${fg[yellow]}%}"
+local blue="%{${fg[blue]}%}"
+local magenta="%{${fg[magenta]}%}"
+local cyan="%{${fg[cyan]}%}"
+local white="%{${fg[white]}%}"
 
+local bold_black="%{${fg_bold[black]}%}"
+local bold_red="%{${fg_bold[red]}%}"
+local bold_green="%{${fg_bold[green]}%}"
+local bold_yellow="%{${fg_bold[yellow]}%}"
+local bold_blue="%{${fg_bold[blue]}%}"
+local bold_magenta="%{${fg_bold[magenta]}%}"
+local bold_cyan="%{${fg_bold[cyan]}%}"
+local bold_white="%{${fg_bold[white]}%}"
+
+
+
+# prompt commands
+local reset="%{${reset_color}%}"
+local directory="%~"
+local prompt_sign="%(!.#.$)"
+local ret_status="%(?.${green}.${red})"
+
+function user_info() {
+    if [[ -n ${HIDE_USER} ]]; then
+        local user="********"
+        local server="********"
+    else
+        local user="%n"
+        local server="%m"
+    fi
+    echo "${USER_INFO_PREFIX}${user}@${server}${USER_INFO_SUFFIX} "
+}
+
+# custom venv prompt
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+function venv_info() {
+    if [[ -n ${VIRTUAL_ENV} ]]; then
+        echo "${VENV_PROMPT_PREFIX}$(basename ${VIRTUAL_ENV})${VENV_PROMPT_SUFFIX} "
+    fi
+}
+
+# prompt variables
 if [[ -a $HOME/.light ]]; then
-    local user_info={240}
+    USER_INFO_PREFIX=${black}
 else
-    local user_info={245}
+    USER_INFO_PREFIX=${white}
 fi
+USER_INFO_SUFFIX="${reset}"
 
-local ret_status="%(?:%{%F${success}%}:%{%F${failure}%}%s)"
-local first_line="%{%F${directory}%}%~ "
-local second_line="%{%F${user_info}%}%n@%M${ret_status}$%{%f%} "
-PROMPT='${first_line}$(git_prompt_info)
-${second_line}'
+VENV_PROMPT_PREFIX="${bold_blue}venv:(${bold_red}"
+VENV_PROMPT_SUFFIX="${bold_blue})${reset}"
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{%F${git}%}git:(%{%F${branch}%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{%F${git}%})"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{%F${git}%}) %{%F${warning}%}✗"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{%f%}"
+ZSH_THEME_GIT_PROMPT_PREFIX="${bold_blue}git:(${bold_red}"
+ZSH_THEME_GIT_PROMPT_CLEAN="${bold_blue})"
+ZSH_THEME_GIT_PROMPT_DIRTY="${bold_blue}) ${yellow}✗"
+ZSH_THEME_GIT_PROMPT_SUFFIX="${reset}"
+
+PROMPT='$(venv_info)${cyan}${directory} $(git_prompt_info)
+$(user_info)${ret_status}${prompt_sign}${reset} '
+
+autoload colors && colors
