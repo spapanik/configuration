5d1a631bc541afb93fad532c5dd84fbe5bac678a
Steph Papanik <spapanik21@gmail.com>
2017-07-06 22:14:56 +0100

    functions to prepend to path

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,1 +1,20 @@
-pb () { curl -F "c=@${1:--}" https://ptpb.pw/ }
+function pb {
+	curl -F "c=@${1:--}" https://ptpb.pw/
+}
+
+function inenv {
+	if [[ -z "${_OLD_PATH+1}" ]]; then
+		_OLD_PATH=${PATH}
+		PATH=${1}:${PATH}
+	else
+		PATH=${1}:${_OLD_PATH}
+	fi
+}
+
+function outenv {
+	if [[ -z "${_OLD_PATH+1}" ]]; then
+		return 1
+	fi
+	PATH=${_OLD_PATH}
+	unset _OLD_PATH
+}
