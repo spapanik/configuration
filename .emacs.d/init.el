b230f1b1dbf47903f679507fe16c8fa9dd99da1a
Steve Papanik <spapanik21@gmail.com>
2018-02-28 13:09:37 +0000

    Remove marmalade

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,8 +22,6 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
