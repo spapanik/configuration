dbbfaf1ccb9be8be91280e0af7fc2057f035864b
Steve Papanik <spapanik21@gmail.com>
2018-05-24 16:34:22 +0100

    Remove proj

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -19,39 +19,6 @@ function outenv {
 	unset _OLD_PATH
 }
 
-function proj {
-	BASE_DIR=~/.config/projects
-	mkdir -p ${BASE_DIR}
-	CREATE=false
-	ACTIVATE=true
-	CD=true
-	NAME=${PWD##*/}
-	while getopts cdn:s opt; do
-		case $opt in
-			c)
-				CREATE=true;;
-			d)
-				ACTIVATE=false;;
-			n)
-				NAME=${OPTARG};;
-			s)
-				CD=false;;
-		esac
-	done
-	PROJ_FILE=${BASE_DIR}/${NAME}
-	if ( ${CREATE} ); then
-		echo ${PWD} > ${PROJ_FILE}
-		echo $(pipenv --venv) >> ${PROJ_FILE}
-		return 0
-	fi
-	if ( ${CD} ); then
-		cd $(sed -n '1p' ${PROJ_FILE})
-	fi
-	if ( ${ACTIVATE} ); then
-		. $(sed -n '2p' ${PROJ_FILE})/bin/activate
-	fi
-}
-
 function cvenv {
 	BASE_DIR=~/.virtualenvs
 	CD=false
@@ -67,6 +34,10 @@ function cvenv {
 				PYTHON=${OPTARG};;
 		esac
 	done
+	if [[ -d ${VENV_DIR} ]]; then
+		echo Virtualenv ${1} already exist, aborting...
+		return 1
+	fi
 	virtualenv ${VENV_DIR} -p ${PYTHON}
 	if ( ${CD} ); then
 		echo ${PWD} >> ${VENV_DIR}/.project
@@ -75,9 +46,23 @@ function cvenv {
 
 function avenv {
 	VENV_DIR=~/.virtualenvs/${1}
-	echo ${VENV_DIR}
+	if [[ ! -d ${VENV_DIR} ]]; then
+		echo Virtualenv ${1} doesn\'t exist, aborting...
+		return 1
+	fi
 	if [[ -r ${VENV_DIR}/.project ]]; then
 		cd $(cat ${VENV_DIR}/.project)
 	fi
 	. ${VENV_DIR}/bin/activate
+}
+
+function dvenv {
+	VENV_DIR=~/.virtualenvs/${1}
+	if [[ ! -d ${VENV_DIR} ]]; then
+		echo Virtualenv ${1} doesn\'t exist, aborting...
+		return 1
+	fi
+	if [[ -w ${VENV_DIR} ]]; then
+		rm -rf ${VENV_DIR}
+	fi
 }
