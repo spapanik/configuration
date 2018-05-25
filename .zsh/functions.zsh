d59264859b2ce518bfc181447b10b70dbc7fc8e5
Steph Papanik <spapanik21@gmail.com>
2018-05-25 07:48:47 +0100

    Don't leak names

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -20,49 +20,57 @@ function outenv {
 }
 
 function cvenv {
-	BASE_DIR=~/.virtualenvs
+	VENV_BASE=~/.local/share/virtualenvs
 	CD=false
-	VENV_DIR=${BASE_DIR}/${PWD##*/}
+	VENV_DIR=${VENV_BASE}/${PWD##*/}
 	PYTHON=$(which python)
 	while getopts an:p: opt; do
 		case $opt in
 			a)
 				CD=true;;
 			n)
-				VENV_DIR=${BASE_DIR}/${OPTARG};;
+				VENV_DIR=${VENV_BASE}/${OPTARG};;
 			p)
 				PYTHON=${OPTARG};;
 		esac
 	done
 	if [[ -d ${VENV_DIR} ]]; then
 		echo Virtualenv ${1} already exist, aborting...
+		unset VENV_BASE VENV_DIR CD PYTHON
 		return 1
 	fi
 	virtualenv ${VENV_DIR} -p ${PYTHON}
 	if ( ${CD} ); then
 		echo ${PWD} >> ${VENV_DIR}/.project
 	fi
+	unset VENV_BASE VENV_DIR CD PYTHON
 }
 
 function avenv {
-	VENV_DIR=~/.virtualenvs/${1}
+	VENV_BASE=~/.local/share/virtualenvs
+	VENV_DIR=${VENV_BASE}/${1}
 	if [[ ! -d ${VENV_DIR} ]]; then
 		echo Virtualenv ${1} doesn\'t exist, aborting...
+		unset VENV_BASE VENV_DIR
 		return 1
 	fi
 	if [[ -r ${VENV_DIR}/.project ]]; then
 		cd $(cat ${VENV_DIR}/.project)
 	fi
 	. ${VENV_DIR}/bin/activate
+	unset VENV_BASE VENV_DIR
 }
 
 function dvenv {
-	VENV_DIR=~/.virtualenvs/${1}
+	VENV_BASE=~/.local/share/virtualenvs
+	VENV_DIR=${VENV_BASE}/${1}
 	if [[ ! -d ${VENV_DIR} ]]; then
 		echo Virtualenv ${1} doesn\'t exist, aborting...
+		unset VENV_BASE VENV_DIR
 		return 1
 	fi
 	if [[ -w ${VENV_DIR} ]]; then
 		rm -rf ${VENV_DIR}
 	fi
+	unset VENV_BASE VENV_DIR
 }
