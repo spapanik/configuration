52b9432ddce6fb28e216807c2bca35b4f7687379
Steph Papanik <spapanik21@gmail.com>
2019-02-16 16:54:26 +0000

    Filter ignored files

--- .zsh/constants.zsh
+++ .zsh/constants.zsh
@@ -2,3 +2,6 @@ export LC_COLLATE=C
 export EDITOR='vi'
 export VISUAL='vi'
 export LESS='-FRXK'
+
+export FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!.git'"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
