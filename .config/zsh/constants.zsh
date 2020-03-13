a7b3e53a37bf3ac444633e7adcad840b3e0abc24
Stephanos Kuma <stephanos.papanikolopoulos@oaknorth.ai>
2020-03-13 12:16:44 +0000

    Add pyenv to config

--- .config/zsh/constants.zsh
+++ .config/zsh/constants.zsh
@@ -3,5 +3,7 @@ export EDITOR='vi'
 export VISUAL='vi'
 export LESS='-FRXK'
 
+export PYENV_SHELL=zsh
+
 export FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!.git'"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,3 +1,19 @@
+command pyenv rehash 2>/dev/null
+pyenv() {
+  local command
+  command="${1:-}"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  rehash|shell)
+    eval "$(pyenv "sh-$command" "$@")";;
+  *)
+    command pyenv "$command" "$@";;
+  esac
+}
+
 function pb {
 	curl -F 'f:1=<-' ix.io
 }
