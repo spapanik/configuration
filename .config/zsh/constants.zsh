549695d6b99f3cd8c9803847e05915da069569d0
Stephanos Kuma <stephanos.papanikolopoulos@oaknorth.ai>
2020-03-13 16:37:35 +0000

    Set the correct tty

--- .config/zsh/constants.zsh
+++ .config/zsh/constants.zsh
@@ -1,9 +1,10 @@
 export LC_COLLATE=C
+
 export EDITOR='vi'
 export VISUAL='vi'
+
 export LESS='-FRXK'
-
+export GPG_TTY=$(tty)
 export PYENV_SHELL=zsh
-
 export FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!.git'"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
