184daa2d4e860efe2a96ff08f9f383509bca7035
Stephanos Kuma <spapanik21@gmail.com>
2020-07-03 10:24:46 +0100

    Add defaults for ripgrep

--- .config/zsh/constants.zsh
+++ .config/zsh/constants.zsh
@@ -4,6 +4,7 @@ export EDITOR='vi'
 export VISUAL='vi'
 
 export LESS='-FRXK'
+export RIPGREP_CONFIG_PATH="$HOME/.config/ripgrep/.rgrc"
 export GPG_TTY=$(tty)
 export PYENV_SHELL=zsh
 export FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!.git'"
