b637ff4104565c9a081fcf919521817f27425de6
Stephanos Kuma <stephanos@kuma.ai>
2022-11-21 09:44:36 +0000

    Use a more readable time format

--- .config/zsh/constants.zsh
+++ .config/zsh/constants.zsh
@@ -14,3 +14,16 @@ export GPG_TTY=$(tty)
 export PYENV_SHELL=zsh
 export FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!.git'"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export TIMEFMT=$'
+%J
+================
+userspace  %*U
+kernel     %*S
+wall       %*E
+----------------
+CPU        %P
+----------------
+space      %K KB
+----------------
+memory     %M KB
+'
