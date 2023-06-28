300c119624e53de7cb90040e095139c795543eff
Stephanos Kuma <stephanos@kuma.ai>
2023-06-28 13:41:57 +0100

    zsh completions ignores case

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -2,5 +2,6 @@ add_to_fpath -p "${HOME}"/.config/zsh/completions
 setopt completealiases
 zstyle ':completion:*' rehash true
 zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}'
 autoload -U compinit
 compinit
