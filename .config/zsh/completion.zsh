cf7d3f35cabeea9153654d559a6df2a597b8bd49
Stephanos Kuma <stephanos@kuma.ai>
2023-10-09 15:44:55 +0100

    Allow completion of aliases

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -1,5 +1,4 @@
 add_to_fpath -p "${HOME}"/.config/zsh/completions
-setopt completealiases
 zstyle ':completion:*' rehash true
 zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}'
